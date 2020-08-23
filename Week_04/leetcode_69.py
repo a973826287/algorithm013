42154061fd9856638f313e329f86d312d7477201
yuqi <a973826287@qq.com>
2020-08-23 15:04:09 +0800

    complete 69

--- Week_04/leetcode_69.py
+++ Week_04/leetcode_69.py
@@ -19,3 +19,17 @@ class Solution:
             else:
                 right = mid - 1
         return res
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        C, x0 = float(x), float(x)
+        while True:
+            xi = 0.5 * (x0 + C / x0)
+            if abs(x0 - xi) < 1e-7:
+                break
+            x0 = xi
+        
+        return int(x0)
