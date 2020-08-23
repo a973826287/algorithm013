e7b5959cb65098e6549d4b1781ee741f2b2c53a7
yuqi <a973826287@qq.com>
2020-08-23 15:03:15 +0800

    complete 69

---
+++ Week_04/leetcode_69.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=69 lang=python3
+#
+# [69] x 的平方根
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        left, right = 1, x
+        res = 0
+        while left <= right:
+            mid = (left + right)//2
+            if mid*mid <=  x:
+                res = mid 
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
